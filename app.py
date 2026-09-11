53f3a32e7f0ebe90b2c2c79ea32c353f059dee77
Seven-Borinz <rinniceone@gmail.com>
2026-09-11 17:32:26 +0700

    Finish Day 4 Create a Note Form

--- app.py
+++ app.py
@@ -1,17 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-
-@app.route("/notes")
-def notes():
-
-    notes_data = [
+notes_data = [
         {
             "id": 1,
             "title": "My First Note",
@@ -27,14 +18,40 @@ def notes():
             "title": "Project Ideas",
             "content": "Build a useful note-taking application."
         }
-    ]
+]
 
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/notes")
+def notes():
     return render_template("notes.html", notes=notes_data)
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
+@app.route("/notes/new", methods=["GET", "POST"])
+def create_note():
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
+
+        new_note = {
+            "id": len(notes_data) + 1,
+            "title": title,
+            "content": content
+        }
+
+        notes_data.append(new_note)
+
+        return redirect(url_for("notes"))
+
+    return render_template("create_note.html")
+       
 
 if __name__ == "__main__":
     app.run(debug=True)
